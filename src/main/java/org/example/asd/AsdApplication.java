fb2520f88663948f2c793de5bdb137784aa353b1
Esha193 <esha.sadarangani@student.uts.edu.au>
2025-09-04 00:35:31 +1000

    login page first page

--- src/main/java/org/example/asd/AsdApplication.java
+++ src/main/java/org/example/asd/AsdApplication.java
@@ -3,11 +3,13 @@ package org.example.asd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+		"org.example.asd",
+		"betterpedia.user"
+})
 public class AsdApplication {
-// http://localhost:8080
+	// http://localhost:8080
 	public static void main(String[] args) {
 		SpringApplication.run(AsdApplication.class, args);
 	}
-
 }
