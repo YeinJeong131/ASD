3bf56af9cbeb1cabe34dd91fec3140f841953b27
Esha193 <esha.sadarangani@student.uts.edu.au>
2025-09-24 16:24:11 +1000

    user management feature

---
+++ src/main/java/org/example/asd/model/Role.java
@@ -0,0 +1,47 @@
+package org.example.asd.model;
+
+import jakarta.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 40)
+    private String name; // ROLE_USER, ROLE_ADMIN
+
+    public Role() {}
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role)) return false;
+        Role role = (Role) o;
+        return Objects.equals(name, role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
