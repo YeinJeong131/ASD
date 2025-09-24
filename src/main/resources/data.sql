e7e5ebbaf295cce7c414d6afef9497233b2ab560
Esha193 <esha.sadarangani@student.uts.edu.au>
2025-09-24 19:07:25 +1000

    database addition -user and roles

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,14 @@
-USE wiki;
+# USE wiki;
+
+SET FOREIGN_KEY_CHECKS=0;
+TRUNCATE TABLE user_roles;
+TRUNCATE TABLE article_authors;
+TRUNCATE TABLE article_tags;
+TRUNCATE TABLE users;
+TRUNCATE TABLE roles;
+TRUNCATE TABLE article;
+SET FOREIGN_KEY_CHECKS=1;
+
 INSERT INTO article(title, body, publish_date, lan) VALUES
 ('DummyData', 'This is dummyText yo yo \n dummy\n yoho\n\n\n\n\ndunno. lol', CURDATE(), 'ENGLISH');
 INSERT INTO article_authors (article_id, author) VALUES (1, 'Nima');
