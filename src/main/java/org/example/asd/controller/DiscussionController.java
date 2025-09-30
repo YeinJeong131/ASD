25aa6a02f7fae11747d4e17f0a8e13f0378505a0
yuhan0113 <2856528583@qq.com>
2025-09-30 18:51:39 +1000

    Discussion feature update

--- src/main/java/org/example/asd/controller/DiscussionController.java
+++ src/main/java/org/example/asd/controller/DiscussionController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 public class DiscussionController {
 
@@ -15,17 +17,20 @@ public class DiscussionController {
         this.commentService = commentService;
     }
 
-    @GetMapping("/discussion")
-    public String showDiscussion(@RequestParam(defaultValue = "1") Long articleId, Model model) {
+    @GetMapping("/discussion/{articleId}")
+    public String showDiscussion(@PathVariable Long articleId, Model model) {
+        List<Comment> comments = commentService.getCommentsByArticleId(articleId);
+        model.addAttribute("comments", comments);
+        model.addAttribute("newComment", new Comment()); // for form binding
         model.addAttribute("articleId", articleId);
-        model.addAttribute("comments", commentService.getCommentsByArticleId(articleId));
-        model.addAttribute("comment", new Comment()); // for form binding
-        return "discussion";
+        return "discussion"; // must match templates/discussion.html
     }
 
-    @PostMapping("/discussion")
-    public String postComment(@ModelAttribute Comment comment) {
+    @PostMapping("/discussion/{articleId}")
+    public String postComment(@PathVariable Long articleId,
+                              @ModelAttribute("newComment") Comment comment) {
+        comment.setArticleId(articleId);
         commentService.saveComment(comment);
-        return "redirect:/discussion?articleId=" + comment.getArticleId();
+        return "redirect:/discussion/" + articleId;
     }
 }
